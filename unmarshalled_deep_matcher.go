31686c35f26eacae92d6ba224680b98b019f34c4
Ben Fuller <bfuller@pivotal.io>
2018-06-22 10:52:07 -0600

    clean up code a bit

--- unmarshalled_deep_matcher.go
+++ unmarshalled_deep_matcher.go
@@ -8,6 +8,9 @@ type UnmarshalledDeepMatcher struct {
 	Subset             bool
 }
 
+type yamlMap map[interface{}]interface{}
+type jsonMap map[string]interface{}
+
 func (matcher *UnmarshalledDeepMatcher) deepEqual(a interface{}, b interface{}) (bool, []interface{}){
 	return matcher.deepEqualRecursive(a, b, false)
 }
@@ -22,20 +25,20 @@ func (matcher *UnmarshalledDeepMatcher) deepEqualRecursive(a interface{}, b inte
 	switch a.(type) {
 	case []interface{}:
 		if (matcher.Ordered && !invertOrdering) || (!matcher.Ordered && invertOrdering){
-			return matcher.deepEqualOrderedList(a, b, errorPath)
+			return matcher.deepEqualOrderedList(a.([]interface{}), b.([]interface{}), errorPath)
 		} else {
-			return matcher.deepEqualUnorderedList(a, b, errorPath)
+			return matcher.deepEqualUnorderedList(a.([]interface{}), b.([]interface{}), errorPath)
 		}
-	case map[string]interface{}:
-		return matcher.deepEqualMap(toInterfaceMap(a.(map[string]interface{})), toInterfaceMap(b.(map[string]interface{})), errorPath)
-	case map[interface{}]interface{}:
-		return matcher.deepEqualMap(a.(map[interface{}]interface{}), b.(map[interface{}]interface{}), errorPath)
+	case jsonMap:
+		return matcher.deepEqualMap(toYamlMap(a.(jsonMap)), toYamlMap(b.(jsonMap)), errorPath)
+	case yamlMap:
+		return matcher.deepEqualMap(a.(yamlMap), b.(yamlMap), errorPath)
 	default:
 		return a == b, errorPath
 	}
 }
 
-func (matcher *UnmarshalledDeepMatcher) deepEqualMap(a map[interface{}]interface{}, b map[interface{}]interface{}, errorPath []interface{}) (bool, []interface{}) {
+func (matcher *UnmarshalledDeepMatcher) deepEqualMap(a yamlMap, b yamlMap, errorPath []interface{}) (bool, []interface{}) {
 	if matcher.Subset {
 		if len(a) > len(b) {
 			return false, errorPath
@@ -60,22 +63,22 @@ func (matcher *UnmarshalledDeepMatcher) deepEqualMap(a map[interface{}]interface
 	return true, errorPath
 }
 
-func (matcher *UnmarshalledDeepMatcher) deepEqualUnorderedList(a interface{}, b interface{}, errorPath []interface{}) (bool, []interface{}) {
-	matched := make([]bool, len(b.([]interface{})))
+func (matcher *UnmarshalledDeepMatcher) deepEqualUnorderedList(a []interface{}, b []interface{}, errorPath []interface{}) (bool, []interface{}) {
+	matched := make([]bool, len(b))
 
 	if matcher.Subset {
-		if len(a.([]interface{})) > len(b.([]interface{})) {
+		if len(a) > len(b) {
 			return false, errorPath
 		}
 	} else {
-		if len(a.([]interface{})) != len(b.([]interface{})) {
+		if len(a) != len(b) {
 			return false, errorPath
 		}
 	}
 
-	for _, v1 := range a.([]interface{}) {
+	for _, v1 := range a {
 		foundMatch := false
-		for j, v2 := range b.([]interface{}) {
+		for j, v2 := range b {
 			if matched[j] {
 				continue
 			}
@@ -94,19 +97,19 @@ func (matcher *UnmarshalledDeepMatcher) deepEqualUnorderedList(a interface{}, b 
 	return true, errorPath
 }
 
-func (matcher *UnmarshalledDeepMatcher) deepEqualOrderedList(a interface{}, b interface{}, errorPath []interface{}) (bool, []interface{}) {
+func (matcher *UnmarshalledDeepMatcher) deepEqualOrderedList(a []interface{}, b []interface{}, errorPath []interface{}) (bool, []interface{}) {
 	if matcher.Subset {
-		if len(a.([]interface{})) > len(b.([]interface{})) {
+		if len(a) > len(b) {
 			return false, errorPath
 		}
 	} else {
-		if len(a.([]interface{})) != len(b.([]interface{})) {
+		if len(a) != len(b) {
 			return false, errorPath
 		}
 	}
 
-	for i, v := range a.([]interface{}) {
-		elementEqual, keyPath := matcher.deepEqualRecursive(v, b.([]interface{})[i], false)
+	for i, v := range a {
+		elementEqual, keyPath := matcher.deepEqualRecursive(v, b[i], false)
 		if !elementEqual {
 			return false, append(keyPath, i)
 		}
@@ -114,8 +117,8 @@ func (matcher *UnmarshalledDeepMatcher) deepEqualOrderedList(a interface{}, b in
 	return true, errorPath
 }
 
-func toInterfaceMap(map1 map[string]interface{}) (map[interface{}]interface{}){
-	convert := make(map[interface{}]interface{})
+func toYamlMap(map1 jsonMap) (yamlMap){
+	convert := make(yamlMap)
 	for key, value := range map1 {
 		convert[key] = value
 	}
