9ed15a17e4d87c7246a61e99d3f5a726940d6046
Ben Fuller <bfuller@pivotal.io>
2018-06-22 10:44:00 -0600

    removed code that doesn't seem to have a purpose

--- expanded_unmarshalled_matchers_suite_test.go
+++ expanded_unmarshalled_matchers_suite_test.go
@@ -5,49 +5,9 @@ import (
 	. "github.com/onsi/gomega"
 
 	"testing"
-	"io/ioutil"
-	"fmt"
-	"os"
 )
 
 func TestUnmarshalledMatchers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "unmarshalledmatchers Suite")
-}
-
-
-
-func readFileContents(filePath string) []byte {
-	f := openFile(filePath)
-	b, err := ioutil.ReadAll(f)
-	if err != nil {
-		panic(fmt.Errorf("failed to read file contents: %v", err))
-	}
-	return b
-}
-
-func openFile(filePath string) *os.File {
-	f, err := os.Open(filePath)
-	if err != nil {
-		panic(fmt.Errorf("failed to open file: %v", err))
-	}
-	return f
-}
-
-
-type myStringer struct {
-	a string
-}
-
-func (s *myStringer) String() string {
-	return s.a
-}
-
-type StringAlias string
-
-type myCustomType struct {
-	s   string
-	n   int
-	f   float32
-	arr []string
 }
